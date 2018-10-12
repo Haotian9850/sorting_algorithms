d4b4ec9e84d4af70d1c1f2e439c866fd8a8edad1
Haotian9850 <haotian9850@gmail.com>
2018-10-12 16:48:49 -0400

    10-12-2018

--- src/RadixSorter.java
+++ src/RadixSorter.java
@@ -7,6 +7,22 @@ public class RadixSorter {
         //caller function for using radix sort
 
     }
-    
+
+    /*Util function to get max out of an array*/
+    private int getMax(int[] nums){
+        int result = nums[0];
+        for(Integer i : nums){
+            if(i > result){
+                result = i;
+            }
+        }
+        return result;
+    }
+
+    /*Util function to count sort a small array*/
+    private void countingSort(int[] nums, int n, int exponent){
+        
+    }
+
 
 }
